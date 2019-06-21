d6950b0efafeca5f84d80938c81a71f440f8b6d2
jopatino <jopatino86@misena.edu.co>
2019-06-21 09:12:58 -0500

    Initial commit

---
+++ WebApplication1/Data/Ejercicio5Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ejercicio5.Models;
+
+    public class Ejercicio5Context : DbContext
+    {
+        public Ejercicio5Context (DbContextOptions<Ejercicio5Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ejercicio5.Models.Categoria> Categoria { get; set; }
+    }
